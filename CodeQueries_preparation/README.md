be8293c45b2f5aa07428dd0044d42d8fa777d568
thepurpleowl <suryathe16@gmail.com>
2023-06-12 23:20:38 +0530

    Refactor data preparation code

---
+++ CodeQueries_preparation/README.md
@@ -0,0 +1,8 @@
+### Steps for data preparation
+1. Compile the proto definitions. For a quick reference, follow this [blog](https://www.freecodecamp.org/news/googles-protocol-buffers-in-python/).  
+`protoc -I=. --python_out=. <proto_path>`
+2. Run `download_and_serialize_dataset.sh`
+
+
+
+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
--- CodeQueries_preparation/data_ingestion/download_and_serialize_dataset.sh
+++ CodeQueries_preparation/data_ingestion/download_and_serialize_dataset.sh
@@ -25,22 +25,22 @@ tar -xvzf py150_files.tar.gz
 
 tar -xvzf data.tar.gz
 
-bazel run //data_ingestion:create_raw_programs -- --data_source=other --source_name=eth_py150_open \
+python run_create_raw_programs_dataset.py --data_source=other --source_name=eth_py150_open \
 --split_name=TRAIN --programs_file_path=$(pwd)/train_program_files_paths.txt \
 --dataset_programming_language=Python --downloaded_dataset_location=$(pwd)/data \
 --save_dataset_location=$(pwd)/train_raw_programs_serialized
 
-bazel run //data_ingestion:create_raw_programs -- --data_source=other --source_name=eth_py150_open \
+python run_create_raw_programs_dataset.py --data_source=other --source_name=eth_py150_open \
 --split_name=VALIDATION --programs_file_path=$(pwd)/dev_program_files_paths.txt \
 --dataset_programming_language=Python --downloaded_dataset_location=$(pwd)/data \
 --save_dataset_location=$(pwd)/dev_raw_programs_serialized
 
-bazel run //data_ingestion:create_raw_programs -- --data_source=other --source_name=eth_py150_open \
+python run_create_raw_programs_dataset.py --data_source=other --source_name=eth_py150_open \
 --split_name=TEST --programs_file_path=$(pwd)/eval_program_files_paths.txt \
 --dataset_programming_language=Python --downloaded_dataset_location=$(pwd)/data \
 --save_dataset_location=$(pwd)/eval_raw_programs_serialized
 
-bazel run //data_ingestion:run_create_queryset -- --labeled_queries_file_path=$(pwd)/$3 \
+python run_create_raw_codeql_queryset.py --labeled_queries_file_path=$(pwd)/$3 \
 --target_programming_language=Python --github_auth=$1:$2 --save_queryset_location=$(pwd)/raw_queries_serialized
 
 rm -rf dev_program_files_paths.txt train_program_files_paths.txt eval_program_files_paths.txt \
